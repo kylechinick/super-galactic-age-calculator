0b527145a045a481a0f907f5d5193f82d89a6ca0
Kyle Chinick <kylechinick@icloud.com>
2022-06-24 15:47:42 -0700

    REFACTOR marsYearsConverter() and test to use an 'ageToConvert' parameter

--- __tests__/biz-func.test.js
+++ __tests__/biz-func.test.js
@@ -1,55 +1,66 @@
 import GalacticDatabase from '../src/js/biz-func.js';
 
 describe('GalacticDatabase', () => {
-  test('Create an instance of the GalacticDatabase class', () => {
-    const sectorOneDataBase = new GalacticDatabase();
-    expect(sectorOneDataBase).toEqual({});
-  });
+  // test('Create an instance of the GalacticDatabase class', () => {
+  //   const sectorOneDataBase = new GalacticDatabase();
+  //   expect(sectorOneDataBase).toEqual({});
+  // });
 
-  test('Create an instance of GalacticDatabase with a defined age property', () => {
-    const sectorOneDataBase = new GalacticDatabase(20);
-    expect(sectorOneDataBase.databaseUserAge).toEqual(20);
-  });
+  // test('Create an instance of GalacticDatabase with a defined age property', () => {
+  //   const sectorOneDataBase = new GalacticDatabase(20);
+  //   expect(sectorOneDataBase.databaseUserAge).toEqual(20);
+  // });
 
-  test('Return a number that is .24 times the value of databaseUserAge via the GalacticDatabase method mercuryYearsConverter', () => {
-    const sectorOneDataBase = new GalacticDatabase(20);
-    expect(sectorOneDataBase.mercuryYearsConverter()).toEqual(4.8);
-  });
+  // test('Return a number that is .24 times the value of databaseUserAge via the GalacticDatabase method mercuryYearsConverter', () => {
+  //   const sectorOneDataBase = new GalacticDatabase(20);
+  //   expect(sectorOneDataBase.mercuryYearsConverter()).toEqual(4.8);
+  // });
 
-  test('Return a number that is .62 times the value of databaseUserAge via the GalacticDatabase method venusYearsConverter', () => {
-    const sectorOneDataBase = new GalacticDatabase(20);
-    expect(sectorOneDataBase.venusYearsConverter()).toEqual(12.4);
-  });
+  // test('Return a number that is .62 times the value of databaseUserAge via the GalacticDatabase method venusYearsConverter', () => {
+  //   const sectorOneDataBase = new GalacticDatabase(20);
+  //   expect(sectorOneDataBase.venusYearsConverter()).toEqual(12.4);
+  // });
 
   test('Return a number that is 1.88 times the value of databaseUserAge via the GalacticDatabase method marsYearsConverter', () => {
     const sectorOneDataBase = new GalacticDatabase(20);
-    expect(sectorOneDataBase.marsYearsConverter()).toEqual(37.6);
+    expect(
+      sectorOneDataBase.marsYearsConverter(sectorOneDataBase.databaseUserAge)
+    ).toEqual(37.6);
   });
 
-  test('Return a number that is 11.86 times the value of databaseUserAge via the GalacticDatabase method jupiterYearsConverter', () => {
-    const sectorOneDataBase = new GalacticDatabase(20);
-    expect(sectorOneDataBase.jupiterYearsConverter()).toEqual(237.2);
-  });
-
-  test('Return the number result of subtracting databaseUserAge from 100 via the GalacticDatabase method yearsRemainingCalculator()', () => {
-    const sectorOneDataBase = new GalacticDatabase(20);
-    expect(sectorOneDataBase.yearsRemainingCalculator()).toEqual(80);
-  });
-
-  // test('Return the number result of subtracting databaseUserAge from 100 via the GalacticDatabase method yearsRemainingCalculator() as a positive number if databaseUserAge is greater than 100 ', () => {
-  //   const sectorOneDataBase = new GalacticDatabase(120);
-  //   expect(sectorOneDataBase.yearsRemainingCalculator()).toEqual(20);
+  // test('Return a number that is 11.86 times the value of databaseUserAge via the GalacticDatabase method jupiterYearsConverter', () => {
+  //   const sectorOneDataBase = new GalacticDatabase(20);
+  //   expect(sectorOneDataBase.jupiterYearsConverter()).toEqual(237.2);
   // });
 
-  test('Return a string stating the positive number of years above 100 that the user has lived based on databaseUserAge', () => {
-    const sectorOneDataBase = new GalacticDatabase(120);
-    expect(sectorOneDataBase.yearsRemainingCalculator()).toEqual(
-      'You have lived 20 years past average life expectancy, keep it up!'
-    );
-  });
+  // test('Return the number result of subtracting databaseUserAge from 100 via the GalacticDatabase method yearsRemainingCalculator()', () => {
+  //   const sectorOneDataBase = new GalacticDatabase(20);
+  //   expect(sectorOneDataBase.yearsRemainingCalculator()).toEqual(80);
+  // });
 
-  test('Return the planet-converted number of years remaining or exeeded against planet-converted average life-span of 100 Earth years', () => {
-    const sectorOneDataBase = new GalacticDatabase(20);
-    expect(sectorOneDataBase.yearsRemainingCalculator(mars)).toEqual(150.4);
-  });
+  // // test('Return the number result of subtracting databaseUserAge from 100 via the GalacticDatabase method yearsRemainingCalculator() as a positive number if databaseUserAge is greater than 100 ', () => {
+  // //   const sectorOneDataBase = new GalacticDatabase(120);
+  // //   expect(sectorOneDataBase.yearsRemainingCalculator()).toEqual(20);
+  // // });
+
+  // test('Return a string stating the positive number of years above 100 that the user has lived based on databaseUserAge', () => {
+  //   const sectorOneDataBase = new GalacticDatabase(120);
+  //   expect(sectorOneDataBase.yearsRemainingCalculator()).toEqual(
+  //     'You have lived 20 years past average life expectancy, keep it up!'
+  //   );
+  // });
+
+  // test('Log the converted average lifespan to the console', () => {
+  //   const sectorOneDataBase = new GalacticDatabase(20);
+  //   expect(sectorOneDataBase.yearsRemainingCalculator(20, 100, 'mars')).toEqual(
+  //     150.4
+  //   );
+  // });
+
+  // test('Return the planet-converted number of years remaining or exceeded against planet-converted average life-span of 100 Earth years', () => {
+  //   const sectorOneDataBase = new GalacticDatabase(20);
+  //   expect(sectorOneDataBase.yearsRemainingCalculator(20, 100, 'mars')).toEqual(
+  //     150.4
+  //   );
+  // });
 });
--- src/js/biz-func.js
+++ src/js/biz-func.js
@@ -5,18 +5,71 @@ export default class GalacticDatabase {
     this.databaseUserAge = databaseUserAge;
   }
 
-  mercuryYearsConverter() {
-    return this.databaseUserAge * 0.24;
+  // mercuryYearsConverter() {
+  //   return this.databaseUserAge * 0.24;
+  // }
+  // venusYearsConverter() {
+  //   return this.databaseUserAge * 0.62;
+  // }
+  marsYearsConverter(ageToConvert) {
+    return parseFloat((ageToConvert * 1.88).toFixed(1));
   }
-  venusYearsConverter() {
-    return this.databaseUserAge * 0.62;
+  // jupiterYearsConverter() {
+  //   return parseFloat((this.databaseUserAge * 11.86).toFixed(1));
+  // }
+  // yearsRemainingCalculator() {
+  //   const userAgeVsExpectancyAge = Math.abs(
+  //     parseFloat((100 - this.databaseUserAge).toFixed(1))
+  //   );
+  //   if (this.databaseUserAge < 100) {
+  //     return userAgeVsExpectancyAge;
+  //   } else {
+  //     return `You have lived ${userAgeVsExpectancyAge} years past average life expectancy, keep it up!`;
+  //   }
+  // }
+
+  averageLifespanConverter(averageEarthLifespan, conversionPlanet) {
+    let convertedAverageLifespan;
+
+    switch (targetConversionPlanet) {
+      case 'mars':
+        convertedAverageLifespan = this.marsYearsConverter(averageLifespan);
+        break;
+
+      default:
+        break;
+    }
+    console.log(convertedAverageLifespan);
   }
-  marsYearsConverter() {
-    return parseFloat((this.databaseUserAge * 1.88).toFixed(1));
-  }
-  jupiterYearsConverter() {
-    return parseFloat((this.databaseUserAge * 11.86).toFixed(1));
-  }
+
+  // yearsRemainingCalculator(targetAge, averageLifespan, targetConversionPlanet) {
+  //   // GET CONVERTED AVERAGE LIFESPAN
+  //   let convertedAverageLifespan;
+
+  //   switch (targetConversionPlanet) {
+  //     case 'mars':
+  //       convertedAverageLifespan = this.marsYearsConverter(averageLifespan);
+  //       break;
+
+  //     default:
+  //       break;
+  //   }
+  //   console.log(convertedAverageLifespan);
+
+  //   // GET CONVERTED USER AGE
+  //   // RETURN CONVERTED USER AGE AS A NUMBER IF LOWER THAN CONVERTED AVERAGE LIFESPAN
+  //   // ELSE RETURN
+
+  //   const userAgeVsExpectancyAge = Math.abs(
+  //     parseFloat((100 - this.databaseUserAge).toFixed(1))
+  //   );
+  //   if (this.databaseUserAge < 100) {
+  //     return userAgeVsExpectancyAge;
+  //   } else {
+  //     return `You have lived ${userAgeVsExpectancyAge} years past average life expectancy, keep it up!`;
+  //   }
+  // }
+
   yearsRemainingCalculator() {
     const userAgeVsExpectancyAge = Math.abs(
       parseFloat((100 - this.databaseUserAge).toFixed(1))
