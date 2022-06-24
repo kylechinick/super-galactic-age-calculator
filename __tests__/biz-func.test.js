070578665055e20a860adeb2b8114ed94d1fb27c
Kyle Chinick <kylechinick@icloud.com>
2022-06-24 16:51:30 -0700

    Uncomment original class instantiation test

--- __tests__/biz-func.test.js
+++ __tests__/biz-func.test.js
@@ -1,15 +1,15 @@
 import GalacticDatabase from '../src/js/biz-func.js';
 
 describe('GalacticDatabase', () => {
-  // test('Create an instance of the GalacticDatabase class', () => {
-  //   const sectorOneDataBase = new GalacticDatabase();
-  //   expect(sectorOneDataBase).toEqual({});
-  // });
+  test('Create an instance of the GalacticDatabase class', () => {
+    const sectorOneDataBase = new GalacticDatabase();
+    expect(sectorOneDataBase).toEqual({});
+  });
 
-  // test('Create an instance of GalacticDatabase with a defined age property', () => {
-  //   const sectorOneDataBase = new GalacticDatabase(20);
-  //   expect(sectorOneDataBase.databaseUserAge).toEqual(20);
-  // });
+  test('Create an instance of GalacticDatabase with a defined age property', () => {
+    const sectorOneDataBase = new GalacticDatabase(20);
+    expect(sectorOneDataBase.databaseUserAge).toEqual(20);
+  });
 
   test('Return a number that is .24 times the value of databaseUserAge via the GalacticDatabase method mercuryYearsConverter()', () => {
     const sectorOneDataBase = new GalacticDatabase(20);
@@ -30,7 +30,7 @@ describe('GalacticDatabase', () => {
 
   test('Return a number that is 11.86 times the value of databaseUserAge via the GalacticDatabase method jupiterYearsConverter()', () => {
     const sectorOneDataBase = new GalacticDatabase(20);
-    expect(sectorOneDataBase.jupiterYearsConverter()).toEqual(237.2);
+    expect(sectorOneDataBase.jupiterYearsConverter(20)).toEqual(237.2);
   });
 
   // test('Return the number result of subtracting databaseUserAge from 100 via the GalacticDatabase method yearsRemainingCalculator()', () => {
