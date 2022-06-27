d34075e0eaf668e28f48d5a78379bd702f913f78
Kyle Chinick <kylechinick@icloud.com>
2022-06-26 17:09:14 -0700

    Add lifespan output support

--- src/index.js
+++ src/index.js
@@ -8,8 +8,34 @@ $(document).ready(function () {
     let userAgeVal = $('#user-age-input').val();
     const sectorOneDataBase = new GalacticDatabase(userAgeVal);
     sectorOneDataBase.databaseUserAge = userAgeVal;
+    $('#earth-age-output').text(userAgeVal);
     $('#mercury-age-output').text(
       sectorOneDataBase.mercuryYearsConverter(userAgeVal)
+    );
+    $('#venus-age-output').text(
+      sectorOneDataBase.venusYearsConverter(userAgeVal)
+    );
+    $('#mars-age-output').text(
+      sectorOneDataBase.marsYearsConverter(userAgeVal)
+    );
+    $('#jupiter-age-output').text(
+      sectorOneDataBase.jupiterYearsConverter(userAgeVal)
+    );
+
+    $('#earth-lifespan-output').text(
+      sectorOneDataBase.yearsRemainingCalculator(userAgeVal, 100, 'earth')
+    );
+    $('#mercury-lifespan-output').text(
+      sectorOneDataBase.yearsRemainingCalculator(userAgeVal, 100, 'mercury')
+    );
+    $('#venus-lifespan-output').text(
+      sectorOneDataBase.yearsRemainingCalculator(userAgeVal, 100, 'venus')
+    );
+    $('#mars-lifespan-output').text(
+      sectorOneDataBase.yearsRemainingCalculator(userAgeVal, 100, 'mars')
+    );
+    $('#jupiter-lifespan-output').text(
+      sectorOneDataBase.yearsRemainingCalculator(userAgeVal, 100, 'jupiter')
     );
     document.getElementById('user-data-form').reset();
   });
